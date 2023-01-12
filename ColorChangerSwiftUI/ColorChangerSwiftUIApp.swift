1bf99f8bbc67e58ae478a9dac8a4512eb8e65795
Stanislav <kaggy8@ya.ru>
2023-01-12 20:44:06 +0300

    Initial Commit

---
+++ ColorChangerSwiftUI/ColorChangerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorChangerSwiftUIApp.swift
+//  ColorChangerSwiftUI
+//
+//  Created by Stanislav Demyanov on 12.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct ColorChangerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
